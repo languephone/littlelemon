099ae4b92976ef26cd6b565d2e5f34789ad8e548
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-07-20 16:57:42 -0400

    Create Xcode project.

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Daniel Shapiro on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
