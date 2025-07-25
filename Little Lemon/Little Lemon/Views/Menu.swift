b32d95b4d2978abda952039098862fb01699a71a
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-07-24 22:16:16 -0400

    Move views into separate folder.

--- Little Lemon/Little Lemon/Views/Menu.swift
+++ Little Lemon/Little Lemon/Views/Menu.swift
@@ -12,10 +12,20 @@ struct Menu: View {
     @State var searchText = ""
 
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Little Lemon")
+                .font(.largeTitle)
             Text("London")
-            Text("Restaurant app description")
+                .font(.title)
+            HStack {
+                Text("We are a modern take on Mediterranean food in the heart of London's financial district focused on traditional recipies served with a modern twist")
+                Image("profile-image-placeholder")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+            }
+            .padding(.horizontal, 20)
             TextField("Search menu", text: $searchText)
             FetchedObjects(predicate: buildPredicate(), sortDescriptors: buildSortDescriptors()) { (dishes: [Dish]) in
                 List {
