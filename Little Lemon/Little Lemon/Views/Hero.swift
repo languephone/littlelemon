d86dfaf2417137121d7462790b774bbe4d91f1d4
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-07-24 22:16:37 -0400

    Break out Hero view to separate file.

---
+++ Little Lemon/Little Lemon/Views/Hero.swift
@@ -0,0 +1,43 @@
+//
+//  Hero.swift
+//  Little Lemon
+//
+//  Created by Daniel Shapiro on 7/23/25.
+//
+
+import SwiftUI
+
+struct Hero: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("Little Lemon")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .fontDesign(.serif)
+                .foregroundStyle(.littleLemonYellow)
+            HStack(alignment: .bottom) {
+                VStack(alignment: .leading) {
+                    Text("London")
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .fontDesign(.serif)
+                        .foregroundStyle(.white)
+                        .padding(.bottom, 15)
+                    Text("We are a modern take on Mediterranean food in the heart of London's financial district, focused on traditional recipies served with a modern twist")
+                        .foregroundStyle(.white)
+                }
+                    Image("profile-image-placeholder")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 150)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+            }
+        }
+        .padding(20)
+        .background(Color.littleLemonGreen)
+    }
+}
+
+#Preview {
+    Hero()
+}
