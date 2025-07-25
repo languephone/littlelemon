821ff81bd38009b4ef381398e663fff31e1a4c4d
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-07-24 22:51:35 -0400

    Include all MenuItem attributes in struct.

--- Little Lemon/Little Lemon/MenuItem.swift
+++ Little Lemon/Little Lemon/MenuItem.swift
@@ -8,7 +8,10 @@
 import Foundation
 
 struct MenuItem: Codable {
+    let id: Int
     let title: String
-    let image: String
+    let description: String
     let price: String
+    let image: String
+    let category: String
 }
