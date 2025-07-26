0610fd936b4d487ad5b3609adf2eeb58cea0adab
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-07-26 15:30:04 -0400

    Add category and description to Dish entity.

--- Little Lemon/Little Lemon/MenuItem.swift
+++ Little Lemon/Little Lemon/MenuItem.swift
@@ -10,8 +10,18 @@ import Foundation
 struct MenuItem: Codable {
     let id: Int
     let title: String
-    let description: String
+    let dishDescription: String
     let price: String
     let image: String
     let category: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "title"
+        case dishDescription = "description"
+        case price = "price"
+        case image = "image"
+        case category = "category"
+    }
 }
+
