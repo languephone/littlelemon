aa49b4a67f99dde8ae14f3d07ae89f89c1cca876
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-07-30 22:03:21 -0400

    Split Onboarding into 3 screens with User model passed in.

--- Little Lemon/Little Lemon/Views/Onboarding.swift
+++ Little Lemon/Little Lemon/Views/Onboarding.swift
@@ -7,41 +7,32 @@
 
 import SwiftUI
 
-let kFirstName = "first name key"
-let kLastName = "last name key"
-let kEmail = "email key"
-let kIsLoggedIn = "kIsLoggedIn"
-
 struct Onboarding: View {
-    @State var firstName = ""
-    @State var lastName = ""
-    @State var email = ""
-    @State var loggedIn = false
+    @StateObject var user: User = User()
+
     var body: some View {
         NavigationView {
             VStack {
-                NavigationLink(destination: Home(), isActive: $loggedIn) {
+                Header()
+                NavigationLink(destination: Menu(), isActive: $user.loggedIn) {
                     EmptyView()
                 }
-                TextField("First Name", text: $firstName)
-                TextField("Last Name", text: $lastName)
-                TextField("email", text: $email)
-                Button("Register") {
-                    if (!firstName.isEmpty && !lastName.isEmpty && !email.isEmpty) {
-                        UserDefaults.standard.set(firstName, forKey: kFirstName)
-                        UserDefaults.standard.set(lastName, forKey: kLastName)
-                        UserDefaults.standard.set(email, forKey: kEmail)
-                        UserDefaults.standard.set(true, forKey: kIsLoggedIn)
-                        loggedIn = true
-                    }
+                VStack {
+                    InputBox(title: "First Name", text: $user.firstName)
+                    InputBox(title: "Last Name", text: $user.lastName)
+                    InputBox(title: "email", text: $user.email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                }
+                NavigationLink(destination: Onboarding2()) {
+                    Text("Next")
                 }
             }
-            .onAppear() {
-                if (UserDefaults.standard.bool(forKey: kIsLoggedIn)) {
-                    loggedIn = true
-                }
-            }
+            .padding()
+            .frame(maxHeight: .infinity, alignment: .top)
+            
         }
+        .environmentObject(user)
     }
 }
 
---
+++ Little Lemon/Little Lemon/Views/Onboarding3.swift
@@ -0,0 +1,38 @@
+//
+//  Onboarding3.swift
+//  Little Lemon
+//
+//  Created by Daniel Shapiro on 7/28/25.
+//
+
+import SwiftUI
+
+struct Onboarding3: View {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        VStack {
+            Text(user.firstName)
+            Text(user.lastName)
+            Text(user.email)
+            
+            Button("Register") {
+                if (!user.firstName.isEmpty && !user.lastName.isEmpty && !user.email.isEmpty) {
+                    UserDefaults.standard.set(user.firstName, forKey: "kFirstName")
+                    UserDefaults.standard.set(user.lastName, forKey: "kLastName")
+                    UserDefaults.standard.set(user.email, forKey: "kEmail")
+                    UserDefaults.standard.set(user.loggedIn, forKey: "kLoggedIn")
+                    user.loggedIn = true
+                }
+            }
+            
+            NavigationLink(destination: Hero(), isActive: $user.loggedIn) {
+                EmptyView()
+            }
+        }
+    }
+}
+
+#Preview {
+    Onboarding3()
+}
